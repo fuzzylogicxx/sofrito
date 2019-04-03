130840771c31216a7e2ac60654c48bf04aafd55a
Laurence Hughes <laurence@fuzzylogic.me>
2019-04-03 15:01:59 +0100

    Added 'use strict' into cookie consent init script. Wrapped it in an IIFE to keep 'use strict' locally scoped.

--- src/js/main/cookieconsentinit.js
+++ src/js/main/cookieconsentinit.js
@@ -1,17 +1,21 @@
-'use strict';
+;(function () {
 
-window.addEventListener("load", function(){
-window.cookieconsent.initialise({
-  "palette": {
-    "popup": {
-      "background": "#000"
-    },
-    "button": {
-      "background": "#f1d600"
-    }
-  },
-  "theme": "classic",
-  "content": {
-    "href": "https://www.wonderworldsoftplay.co.uk/cookie-policy/"
-  }
-})});
+    'use strict';
+
+    window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+      "palette": {
+        "popup": {
+          "background": "#000"
+        },
+        "button": {
+          "background": "#f1d600"
+        }
+      },
+      "theme": "classic",
+      "content": {
+        "href": "https://www.wonderworldsoftplay.co.uk/cookie-policy/"
+      }
+    })});
+
+})();
